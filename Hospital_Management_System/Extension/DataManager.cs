1bbee7b7b3c79aa8c282ae55ab81877f72d0553b
Suraj <surajkanse16@gmail.com>
2023-12-26 18:28:27 +0530

    1234

---
+++ Hospital_Management_System/Extension/DataManager.cs
@@ -0,0 +1,34 @@
+﻿
+using Hospital_Management_System.HospitalDataManager.DAL;
+using Hospital_Management_System.HospitalDataManager.IDAL;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace Hospital_Management_System.Extension
+{
+    public static class DataManager
+    {
+        public static IServiceCollection AddAppSetting(this IServiceCollection services)
+        {
+            services.AddScoped<IDBManager>(AddDBManager);
+            //services.AddScoped<IEmployeeBAL, EmployeeBAL>;
+            //services.AddScoped<IEmployeeBAL, EmployeeBAL>();
+
+            return services;
+        }
+        internal static IDBManager AddDBManager(IServiceProvider serviceProvider)
+        {
+            IConfiguration Configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+            string dbconstr = Configuration.GetConnectionString("DefaultConnection");
+            return GetDBManager(dbconstr);
+
+
+        }
+        public static IDBManager GetDBManager(string connectionString)
+        {
+            DbConnection dbconn = new MySqlConnection(connectionString);
+            return new DBManager(dbconn);
+        }
+    }
+}
---
+++ Hospital_Management_System/HospitalDataManager/IDAL/IDBManager.cs
@@ -0,0 +1,26 @@
+﻿
+using Hospital_Management_System.HospitalDataManager.DAL;
+using System.Data;
+namespace Hospital_Management_System.HospitalDataManager.IDAL
+{
+    public interface IDBManager
+    {
+        HospitalDataManager.DAL.DBManager InitDbCommand(string cmd);
+        HospitalDataManager.DAL.DBManager InitDbCommand(string cmd, CommandType cmdtype);
+        HospitalDataManager.DAL.DBManager AddCMDParam(string parametername, object value);
+        HospitalDataManager.DAL.DBManager AddCMDParam(string parametername, object value, DbType dbtype);
+        HospitalDataManager.DAL.DBManager AddCMDOutParam(string parametername, DbType dbtype, int iSize = 0);
+
+        T GetOutParam<T>(string paramname);
+        DataTable ExecuteDataTable();
+        DataSet ExecuteDataSet();
+
+        object? ExecuteScalar();
+
+        Task<object?> ExecuteScalarAsync();
+        int ExecuteNonQuery();
+        Task<int> ExecuteNonQueryAsync();
+        HospitalDataManager.DAL.DBManager InitDbCommandText(string cmd);
+
+    }
+}
